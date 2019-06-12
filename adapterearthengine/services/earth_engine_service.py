d43c9b327e09558679d393c1fff97387307fff5e
Enrique Cornejo <enrique@cornejo.me>
2019-06-12 09:52:15 +0200

    Fix -- matches functionality of sql2gee module

--- adapterearthengine/services/earth_engine_service.py
+++ adapterearthengine/services/earth_engine_service.py
@@ -2,15 +2,15 @@ import logging
 
 from adapterearthengine.errors import GEEQueryError
 from sql2gee import SQL2GEE
-
+from sql2gee.utils.jsonSql import JsonSql
 
 def execute_query(query, geojson=None):
     logging.info('Executing Query: '+query)
 
     try:
-        q = SQL2GEE(sql=query, geojson=geojson, flags=None)
+        q = SQL2GEE(JsonSql(query).to_json(), geojson=geojson)
+        logging.info(f'q: {q}')
         response = q.response
     except Exception as error:
         raise GEEQueryError(error)
-
     return response
